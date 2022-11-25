eb4c4c09ea459c74749eb73eccb7465dab5ea55d
ZiweiZhang0304 <zz112@uchicago.edu>
2022-11-25 15:16:21 -0600

    updated func

--- exp_script_E1.js
+++ exp_script_E1.js
@@ -526,7 +526,7 @@ function save_data_csv() {
 jsPsych.init({
     timeline: timeline,
     display_element: 'display_stage',
-    preload_images: preload_list,
+    preload_images: pre_load_list,
     on_finish: function () {
         save_data_csv();
     }
